01e17bf1f6af64a2f50cbad4b90ae98ab32c3c96
jackmqiu <jackqiu2016@gmail.com>
2018-02-04 08:11:47 -0800

    get all users and insert user functions added

--- src/trash-truck/db/index.js
+++ src/trash-truck/db/index.js
@@ -1,5 +1,6 @@
-const config = require('./config.js');
 var mongoose = require('mongoose');
+const User = require('../models/user');
+
 mongoose.connect('mongodb://localhost:27017');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -7,3 +8,18 @@ db.once('open', function() {
   // we're connected!
   console.log('mongodb connected');
 });
+
+let insertUser = (firstName, lastName, address) => {
+  let newUser = {
+    firstName: firstName,
+    lastName: lastName,
+    streetAddress: address,
+  }
+  User.create(newUser, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+
+        }
+    });
+}
